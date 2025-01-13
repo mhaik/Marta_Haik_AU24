2c1c0159c2cc516e82c188ca4b28e5de9e4098a1
Marta Haik <marta.haik@outlook.com>
2025-01-13 22:13:57 +0100

    Completed Session 1 hometasks

--- Python-Course/Session_1/module_1.py
+++ Python-Course/Session_1/module_1.py
@@ -2,35 +2,58 @@ from typing import List
 
 
 def task_1(array: List[int], target: int) -> List[int]:
-    """
-    Write your code below
-    """
-    pass
+    seen_numbers = {}
+    for num in array:
+        complement = target - num
+        if complement in seen_numbers:
+            return [complement, num]
+        seen_numbers[num] = True
+    return []
 
 
 def task_2(number: int) -> int:
-    """
-    Write your code below
-    """
-    pass
+    reversed_number = 0
+    is_negative = number < 0
+    number = abs(number)
+
+    while number > 0:
+        digit = number % 10
+        reversed_number = reversed_number * 10 + digit
+        number //= 10
+    return -reversed_number if is_negative else reversed_number
 
 
 def task_3(array: List[int]) -> int:
-    """
-    Write your code below
-    """
-    pass
+    for i in range(len(array)):
+        index = abs(array[i]) - 1
+        if array[index] < 0:
+            return abs(array[i])
+        array[index] = -array[index]
+    return -1
 
 
 def task_4(string: str) -> int:
-    """
-    Write your code below
-    """
-    pass
+    roman_to_int = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+    total = 0
+    prev_value = 0
+
+    for char in reversed(string):
+        current_value = roman_to_int[char]
+        if current_value < prev_value:
+            total -= current_value
+        else:
+            total += current_value
+        prev_value = current_value
+
+    return total
 
 
 def task_5(array: List[int]) -> int:
-    """
-    Write your code below
-    """
-    pass
+    smallest = array[0]
+    for num in array[1:]:
+        if num < smallest:
+            smallest = num
+    return smallest
